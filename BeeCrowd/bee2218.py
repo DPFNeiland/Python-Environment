20e209e8b50eca483ce15a8a3f806dc01759ec0e
Neiland <rodrigo.naohan@gmail.com>
2025-03-20 15:36:23 -0300

    fs

---
+++ BeeCrowd/bee2218.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+def retas(n):
+
+    # if(n==1):
+    #     return 2
+    
+    # if(listada[i]):
+    #     return lista[i]
+    # else:
+    #     listada[i] = True
+    #     lista[i] = retas(n-1) + n
+    #     return lista[i]
+
+    resp = 2
+
+    for i in range (1,n):
+        resp+-n
+    
+    return resp
+        
+
+T = int(input())
+
+lista = [0,2]
+listada = [False]
+
+for i in range(0,T):
+
+    N = int(input())
+
+    
+    print(retas(N))
