d5f3be9cad6d038fa3fda02f1f1612fe3eaa81a5
Neiland <rodrigo.naohan@gmail.com>
2025-06-04 19:28:30 -0300

    Treino com Questões do ano de 2023 e 2022 da OBI

---
+++ OBI/2023/1faseSenior/subsequencia.py
@@ -0,0 +1,16 @@
+
+
+
+A, B = input().split()
+
+A = int(A)
+B = int(B)
+
+
+sequencia = str(input())
+subsequencia = str(input())
+
+print(sequencia)
+print(subsequencia)
+
+print(f"{"S" if subsequencia in sequencia else "N"}")
