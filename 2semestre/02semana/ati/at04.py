d178f2019288f17b51c74a4ad48cbaa4efb5f6df
Neiland <rodrigo.naohan@gmail.com>
2025-08-22 08:28:37 -0300

    Update at04.py

--- 2semestre/02semana/ati/at04.py
+++ 2semestre/02semana/ati/at04.py
@@ -1,6 +1,8 @@
 
 def mult(m: int, n: int):
-    if n == 1:
+    if m < n:
+        return mult(n,m)
+    elif n == 1:
         return m
     return m + mult(m,n-1)
 
@@ -8,8 +10,6 @@ def main():
     m = int(input())
     n = int(input())
     
-    if n > m:
-        m, n = n, m
         
     print(mult(m,n)) 
     
