becc1e256d2f9ab7171f355642b9c4d46f0e424e
Neiland <rodrigo.naohan@gmail.com>
2026-02-23 23:26:09 -0300

    Segunda Questão da lista do Selmini

--- 3semestre/Listas/Lista01/Segundo.py
+++ 3semestre/Listas/Lista01/Segundo.py
@@ -11,3 +11,82 @@
 # c) Caso todos os valores apresentam a mesma frequência imprimir a mensagem "Todos os 
 # valores apresentam a mesma frequência". 
 # d) O programa não pode usar recursividade.
+
+
+
+def calcular_valor_mais_visto(vector: list[int]):
+    maxi = 1
+    qtd = {}
+    val = set()
+
+    for ele in vector:
+        if ele in qtd:
+            qtd[ele] += 1
+            
+            if qtd[ele] == maxi:
+                val.append(ele)
+            
+            if qtd[ele] > maxi:
+                val = []
+                val.append(ele)
+                maxi += 1
+
+        else:
+            qtd[ele] = 1
+
+    if len(val) == 1:
+        return f"R: {val[0]} -> Freq. Associada: {maxi}"
+
+    if len(val) == len(qtd):
+        return "Todos os valores apresentam a mesma frequência"
+
+    return "Existe mais de 1 valor com a mesma frequência"
+
+
+def main():
+    n = int(input())
+    v = []
+    for _ in range(n):
+        v.append(int(input()))
+    
+    print(calcular_valor_mais_visto(v))
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Por haver apenas um for, a complexidade desse algoritmo é O(n) 
+
+# Possíveis entradas:
+# 7
+# 5
+# 5
+# 5
+# 3
+# 3
+# 4
+# 4
+
+
+# 8
+# 2
+# 2
+# 2
+# 2
+# 3
+# 3
+# 3
+# 3
+
+# 9
+# 2
+# 2
+# 2
+# 2
+# 3
+# 3
+# 3
+# 3
+# 1
+
